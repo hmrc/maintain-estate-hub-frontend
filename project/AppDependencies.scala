07b45f3e244530122d7fe0133d8be8b1f7f0e44f
Victor Osipov <osipovvictor1994@gmail.com>
2023-08-17 09:27:16 +0100

    DDCE-4448 - Upgrade mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,12 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "7.20.0"
+  val bootstrapVersion = "7.21.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.16.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28"
