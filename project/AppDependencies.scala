f41244613c5b6b4731ef2d57112be4b390a39b7a
garytate123 <garytatelufc@gmail.com>
2024-02-29 17:08:54 +0000

    DDCE-5381 Dependency updates for Tudor crown change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,18 +6,18 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"     % "8.5.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.16.0",
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
+    "org.scalatest"               %% "scalatest"                % "3.2.18",
+    "org.jsoup"                   %  "jsoup"                    % "1.17.2",
+    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.18.0",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
+    "com.github.tomakehurst"      %  "wiremock-standalone"      % "3.0.1",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8",
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion
