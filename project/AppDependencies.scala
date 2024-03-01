98c4678da42f0a3eb705d99350e94ffc28d47e41
garytate123 <garytatelufc@gmail.com>
2024-03-01 10:36:33 +0000

    DDCE-5381 Dependency updates for Tudor crown change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.jsoup"                   %  "jsoup"                    % "1.17.2",
     "org.scalatestplus"           %% "mockito-4-11"             % "3.2.18.0",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "3.0.1",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.4.2",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8",
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion
