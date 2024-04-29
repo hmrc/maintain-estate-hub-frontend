2e7847bf3337956343b1c232806f4e5e423574f9
EdCope <87376121+EdCope@users.noreply.github.com>
2024-04-29 12:33:41 +0100

    DDCE-5220: Play 3.0 Migration

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,27 +1,22 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "7.21.0"
+
+  private val bootstrapVersion = "8.5.0"
 
   val compile: Seq[ModuleID] = Seq(
-    play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"     % "8.5.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
-    "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "1.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "8.5.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
+    "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"               %% "scalatest"                % "3.2.18",
     "org.jsoup"                   %  "jsoup"                    % "1.17.2",
-    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.18.0",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.4.2",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
-    "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8",
-    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion
-
+    "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
--- test/views/WhatIsNextViewSpec.scala
+++ test/views/WhatIsNextViewSpec.scala
@@ -52,7 +52,7 @@ class WhatIsNextViewSpec extends ViewBehaviours {
         val doc = asDocument(applyView(form))
 
         for (option <- WhatIsNext.options) {
-          assertContainsRadioButton(doc, option._1.id, "value", option._1.value, false)
+          assertContainsRadioButton(doc, option._1.id, "value", option._1.value, isChecked = false)
           if (option._2.nonEmpty) assertRadioButtonContainsHint(doc, option._1.id + "-item-hint", messages(option._2))
         }
       }
@@ -66,11 +66,11 @@ class WhatIsNextViewSpec extends ViewBehaviours {
 
           val doc = asDocument(applyView(form.bind(Map("value" -> s"${option._1.value}"))))
 
-          assertContainsRadioButton(doc, option._1.id, "value", option._1.value, true)
+          assertContainsRadioButton(doc, option._1.id, "value", option._1.value, isChecked = true)
           if (option._2.nonEmpty) assertRadioButtonContainsHint(doc, option._1.id + "-item-hint", messages(option._2))
 
           for (unselectedOption <- WhatIsNext.options.filterNot(o => o == option)) {
-            assertContainsRadioButton(doc, unselectedOption._1.id, "value", unselectedOption._1.value, false)
+            assertContainsRadioButton(doc, unselectedOption._1.id, "value", unselectedOption._1.value, isChecked = false)
             if (unselectedOption._2.nonEmpty) assertRadioButtonContainsHint(doc, unselectedOption._1.id + "-item-hint", messages(unselectedOption._2))
           }
         }
