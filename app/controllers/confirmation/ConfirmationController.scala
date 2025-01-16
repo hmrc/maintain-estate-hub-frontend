98ab128961b4c8dcca7638c5e65d94164d322734
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-01-16 13:47:05 +0000

    DDCE-5905 - pr bot comment

--- app/controllers/confirmation/ConfirmationController.scala
+++ app/controllers/confirmation/ConfirmationController.scala
@@ -42,7 +42,7 @@ class ConfirmationController @Inject()(
 ) extends FrontendBaseController with I18nSupport with Logging {
 
   private def personalRepName(personalRepresentative: PersonalRepresentativeType)
-                             (implicit request: RequestHeader): String = {
+                             (implicit request: Request[_]): String = {
     personalRepresentative match {
       case PersonalRepresentativeType(Some(EstatePerRepIndType(name, _, _, _, _, _, _, _)), None) =>
         name.displayName
