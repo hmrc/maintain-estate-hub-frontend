d4c2e764c7a1db046abbd1b137305808e6c880b8
83694362+PaulC-TDS@users.noreply.github.com <83694362+PaulC-TDS@users.noreply.github.com>
2025-05-09 10:28:50 +0100

    DDCE-6139: Welsh language and dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ object AppDependencies {
   private val bootstrapVersion = "9.6.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.4.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.6.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "11.10.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.2.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"                   %  "jsoup"                    % "1.18.3",
+    "org.jsoup"                   %  "jsoup"                    % "1.20.1",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion
