6f0da0838cef1a913749093635c7a446f2aa29fa
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-10-24 11:36:32 +0100

    DDCE-6820 - gov uk rebranding

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
   private val bootstrapVersion = "10.2.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.9.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.7.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.10.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.18.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0"
