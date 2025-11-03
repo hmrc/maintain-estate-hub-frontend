3b3a04f1fd7a881aa0ed633c29b22e09a951baa3
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2025-11-03 09:13:39 +0000

    DDCE-7578 bump bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
+  private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.10.0",
