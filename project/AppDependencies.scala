844266a9af0cad762fd1cec8e03085bdd8d050ed
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-12-19 10:55:52 +0000

    DDCE-7764 - pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,9 +5,9 @@ object AppDependencies {
   private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.10.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.19.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.24.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0"
   )
