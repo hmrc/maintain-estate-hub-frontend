971970381aace10259be4cf1e87597a036fe866d
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-12-22 12:56:42 +0000

    DDCE-7764 - updating dependency for fixing sign out link

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.11.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.24.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.25.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0"
